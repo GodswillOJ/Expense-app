c62857a8c44e6a45cd4c79a2b2a6069742a458e1
GodswillOJ <110397088+GodswillOJ@users.noreply.github.com>
2023-01-01 00:01:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require("express");
 const app = express();
 const port = 7000;
 
-app.use(express.static(__dirname + "docs"))
+app.use(express.static(__dirname + "/docs"))
 app.use(express.static("public"))
 app.use("/images", express.static(__dirname + "public/images"))
 app.use("/css", express.static(__dirname + "public/css"))
@@ -18,7 +18,7 @@ app.set("views", "./")
 app.set("view engine", "ejs")
 
 app.get("", (req, res) => {
-    res.render("index.ejs")
+    res.render("/docs/index.ejs")
 })
 app.get("", (req, res) => {
     res.sendFile(__dirname + "/public/css/styles.css")
