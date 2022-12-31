b4f72b7e789647842eb94131b895211d45f0df7c
GodswillOJ <110397088+GodswillOJ@users.noreply.github.com>
2022-12-31 19:23:16 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+// git clone repo_url
+// To update your git repo from your terminal, you need the following commands
+// git add .
+// git commit -m "your commit message goes here"
+// git push origin main
+
+const express = require("express");
+const app = express();
+const port = 7000;
+
+
+app.use(express.static("public"))
+app.use("/images", express.static(__dirname + "public/images"))
+app.use("/css", express.static(__dirname + "public/css"))
+app.use("/js", express.static(__dirname + "public/js"))
+
+app.set("views", "./")
+app.set("view engine", "ejs")
+
+app.get("", (req, res) => {
+    res.render("index.ejs")
+})
+app.get("", (req, res) => {
+    res.sendFile(__dirname + "/public/css/styles.css")
+})
+
+app.listen(port, () => console.info(`listening on port ${port}`))
